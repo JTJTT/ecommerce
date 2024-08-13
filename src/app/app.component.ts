2a6ae6f6d99ebe9e559bc8d00c344bc7744ac402
JTJTT <jt.tran911@gmail.com>
2024-08-12 23:56:35 -0400

    change app routes for homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GlobalHeaderComponent } from './common/components/global-header/global-header.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GlobalHeaderComponent,
+     FormsModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
